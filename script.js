50b232b4798114ca527f4e6a7c92ee170f89e57b
chegleev <alekseypro@list.ru>
2021-11-07 23:31:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+var words = document.querySelector(".words");
+var start = document.getElementById("start");
+var svet = document.getElementById("svet");
+
+var rec = new SpeechRecognition();
+rec.interimResults = true;
+
+var p = document.createElement("p");
+p.innerHTML = "...";
+words.appendChild(p);
+
+start.addEventListener("click", function() {
+    rec.start();
+    this.disabled = true;
+    this.innerHTML = "ГОВОРИТЕ...";
+});
+
+rec.addEventListener("result", function(e) {
+    var text = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
+
+    if(text === "Включить свет") {
+        svet.style.background = "#ffff00";
+    } else if (text === "Выключить свет") {
+        svet.style.background = "#000000";
+    }
+    
+    p.innerHTML = text;
+});
+
+rec.addEventListener("end", function(e) {
+    rec.start();
+});
